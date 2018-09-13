c77108ac3991c3949f1168c09ba57e093f32c79f
Markus Walther <m.walther97@gmail.com>
2018-09-14 00:29:10 +0200

    Add installer

---
+++ .dotfiles_install.sh
@@ -0,0 +1,18 @@
+git clone --bare git@github.com:markus-wa/dotfiles.git $HOME/.dotfiles
+
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+
+dotfiles checkout
+if [ $? = 0 ]; then
+	echo "Checked out dotfiles.";
+else
+	echo "Backing up pre-existing dotfiles.";
+	mkdir -p .dotfiles-backup
+	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
---
+++ README.md
@@ -0,0 +1,8 @@
+# dotfiles
+
+These are my personal dotfiles.
+
+## Installing
+
+	curl -s https://raw.githubusercontent.com/markus-wa/dotfiles/master/.dotfiles_install.sh | sh	
+
