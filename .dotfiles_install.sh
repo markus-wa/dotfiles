028e4aeae249faf2306a29ecddb1e11acc0bbbe9
Markus Walther <m.walther97@gmail.com>
2020-03-02 20:07:39 +0000

    no ssh for cloning

--- .dotfiles_install.sh
+++ .dotfiles_install.sh
@@ -20,7 +20,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt install -y python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
 
-git clone --bare git@github.com:markus-wa/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/markus-wa/dotfiles.git $HOME/.dotfiles
 
 function dotfiles {
     git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
@@ -32,7 +32,7 @@ if [ $? = 0 ]; then
 	echo "Checked out dotfiles.";
 else
 	echo "Backing up pre-existing dotfiles.";
-	mkdir -p .dotfiles-backup
+	mkdir -p ~/.dotfiles-backup
 	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 
