9dc4b123fc6d359e2014e8f933f8e6a9002f99e4
Markus Walther <m.walther97@gmail.com>
2020-03-02 20:01:54 +0000

    apt install -y

--- .dotfiles_install.sh
+++ .dotfiles_install.sh
@@ -7,17 +7,17 @@ sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt update
 
 # git + zsh
-sudo apt install git zsh
+sudo apt install -y git zsh
 
 # emacs + spacemacs
-sudo apt install emacs26
+sudo apt install -y emacs26
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # thefuck
-sudo apt install python3-dev python3-pip python3-setuptools
+sudo apt install -y python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
 
 git clone --bare git@github.com:markus-wa/dotfiles.git $HOME/.dotfiles
