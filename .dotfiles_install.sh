0eced310066d6766575ede1cb23579240f8cb005
Markus Walther <m.walther97@gmail.com>
2020-03-02 19:26:53 +0000

    fix install script

--- .dotfiles_install.sh
+++ .dotfiles_install.sh
@@ -1,11 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 git clone --bare git@github.com:markus-wa/dotfiles.git $HOME/.dotfiles
 
 function dotfiles {
    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
+# install stuff
+sudo apt update
 
+# emacs + spacemacs
+sudo apt install emacs26 curl
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# thefuck
+sudo apt install python3-dev python3-pip python3-setuptools
+sudo pip3 install thefuck
+
+# checkout dotfiles
 dotfiles checkout
 if [ $? = 0 ]; then
 	echo "Checked out dotfiles.";
@@ -18,5 +32,6 @@ fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
 
+# start emacs service
 systemctl enable --user emacs
 systemctl start --user emacs
