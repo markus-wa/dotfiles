069115e17def643245ac2001b3472ec8a0ea5875
Markus Walther <m.walther97@gmail.com>
2020-03-02 20:26:40 +0000

    fix backup process v3

--- .dotfiles_install.sh
+++ .dotfiles_install.sh
@@ -35,7 +35,7 @@ else
 	  echo "Backing up pre-existing dotfiles.";
     set -e
     mkdir -p ~/.dotfiles-backup
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} ~/.dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv ~/{} ~/.dotfiles-backup/{}
 fi;
 set -e
 
