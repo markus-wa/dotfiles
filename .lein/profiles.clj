a128bbe8762c4238d0f9bc4004723818ae29e5fb
Markus Walther <markus.walther@kaluza.com>
2021-02-05 14:26:50 +0000

    lein improvements

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user
- {:dependencies [[hashp "0.2.0"]]
+ {:dependencies [[hashp "0.2.0"]
+                 [vlaaad/reveal "1.3.194"]
+                 [refactor-nrepl "2.5.0"]]
   :injections [(require 'hashp.core)]
-  :plugins [[venantius/ultra "0.6.0"]]}}
+  :plugins [[venantius/ultra "0.6.0"]]
+  :repl-options {:nrepl-middleware [vlaaad.reveal.nrepl/middleware,refactor-nrepl.middleware/wrap-refactor]}}}
--- .zshenv
+++ .zshenv
@@ -1,6 +1,7 @@
 EDITOR="emacsclient -t"
 VISUAL="emacsclient -c -n -a runemacs"
 export BROWSER="firefox"
+export LEIN_USE_BOOTCLASSPATH=no
 
 source $HOME/.zshenv_local
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
