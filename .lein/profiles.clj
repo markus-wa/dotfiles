75208d9f3f6c9201ce6a7628e60b88d1ed62abf1
Markus Walther <mark@saiko.tech>
2022-01-21 11:42:03 +0000

    lein: remove reveal

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,4 +4,4 @@
                  [refactor-nrepl "2.5.0"]]
   :injections [(require 'hashp.core)]
   :plugins [[venantius/ultra "0.6.0"]]
-  :repl-options {:nrepl-middleware [vlaaad.reveal.nrepl/middleware,refactor-nrepl.middleware/wrap-refactor]}}}
+  :repl-options {:nrepl-middleware [refactor-nrepl.middleware/wrap-refactor]}}}
