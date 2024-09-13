c99cc9f6261f72f0e5bf17e05c4ad8fc0f783233
Markus Walther <mark@saiko.tech>
2024-09-13 16:18:07 +0100

    remove old stuff

--- .lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user
- {:dependencies [[hashp "0.2.0"]
-                 [vlaaad/reveal "1.3.194"]
-                 [refactor-nrepl "2.5.0"]]
-  :injections [(require 'hashp.core)]
-  :plugins [[venantius/ultra "0.6.0"]]
-  :repl-options {:nrepl-middleware [refactor-nrepl.middleware/wrap-refactor]}}}
